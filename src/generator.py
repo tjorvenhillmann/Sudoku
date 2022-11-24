c8da3e9b64a2806fa1b5ae8e3736e800cf909563
tjorvenhillmann <tjorven.hillmann@web.de>
2022-11-24 21:20:29 +0100

    Corrected some lines in solverGrid function

--- src/generator.py
+++ src/generator.py
@@ -30,48 +30,55 @@ class GENERARTOR:
         return True
 
     def solveGrid(self):
-        grid = self.grid
         # Find next empty cell to solve
         for i in range(0,81):
             row = i//9 # Needed to get on 9th cell row = 1 and thats the 2nd row of the grid 
             col = i%9  # No integer divison because columns are not affected by line break 
-            if grid[row][col] == 0:
+            if self.grid[row][col] == 0:
                 for value in range(1,10):
                     # Check that this number is not alreday used in this row
-                    if value not in grid[row]:
+                    if value not in self.grid[row]:
                         # Check if this number is not already used in this column
-                        if value not in (grid[0][col],grid[1][col],grid[2][col],grid[3][col],grid[4][col],grid[5][col],grid[6][col],grid[7][col],grid[8][col]):
+                        if value not in (self.grid[0][col],
+                                         self.grid[1][col],
+                                         self.grid[2][col],
+                                         self.grid[3][col],
+                                         self.grid[4][col],
+                                         self.grid[5][col],
+                                         self.grid[6][col],
+                                         self.grid[7][col],
+                                         self.grid[8][col]):
                             # Identify in which of the 9 squares were are working
                             # Empty square is an a 2D-list 
                             square = []
                             # Top squares 3x3
                             if row < 3: 
                                 if col < 3:
-                                    square = [grid[i][0:3] for i in range(0,3)]
+                                    square = [self.grid[i][0:3] for i in range(0,3)]
                                 elif col < 6:
-                                    square = [grid[i][3:6] for i in range(0,3)]
+                                    square = [self.grid[i][3:6] for i in range(0,3)]
                                 else:
-                                    square = [grid[i][6:9] for i in range(0,3)]
+                                    square = [self.grid[i][6:9] for i in range(0,3)]
                             # Middle squares 3x3
                             elif row < 6:  
                                 if col < 3:
-                                    square = [grid[i][0:3] for i in range(0,3)]
+                                    square = [self.grid[i][0:3] for i in range(0,3)]
                                 elif col < 6:
-                                    square = [grid[i][3:6] for i in range(0,3)]
+                                    square = [self.grid[i][3:6] for i in range(0,3)]
                                 else:
-                                    square = [grid[i][6:9] for i in range(0,3)]
+                                    square = [self.grid[i][6:9] for i in range(0,3)]
                             # Bottom squares 3x3
                             else: 
                                 if col < 3:
-                                    square = [grid[i][0:3] for i in range(0,3)]
+                                    square = [self.grid[i][0:3] for i in range(0,3)]
                                 elif col < 6:
-                                    square = [grid[i][3:6] for i in range(0,3)]
+                                    square = [self.grid[i][3:6] for i in range(0,3)]
                                 else:
-                                    square = [grid[i][6:9] for i in range(0,3)]
+                                    square = [self.grid[i][6:9] for i in range(0,3)]
                             
                             # Check that this number is not already be used on this 3x3 square
-                            if value not in (square[0], + square[1] + square[2]):
-                                grid[row][col] = value
+                            if value not in (square[0] + square[1] + square[2]):
+                                self.grid[row][col] = value
                                 if self.checkGrid():
                                     self.counter += 1
                                     break
@@ -79,7 +86,7 @@ class GENERARTOR:
                                     if self.solveGrid():
                                         return True 
                 break
-        grid[row][col] = 0
+        self.grid[row][col] = 0
 
     def fillGrid(self):
         # Find next empty cell to solve
