5579998c27d186b3c6da1ba505b8e6cddc6fd1a3
julez157 <julian.kowsky@icloud.com>
2023-01-05 20:03:05 +0100

    commentet and cleaned generaror

--- src/dev_gui/Scalable/generatorV2.py
+++ src/dev_gui/Scalable/generatorV2.py
@@ -2,8 +2,18 @@ from random import randint, shuffle
 from time import time
 
 class Generator:
+    '''
+    Generator Class is the logic of a Sudoku Game. The Class contains the main funkctions of the Sudoku.
+    The Generator is build with the fillGrid and the reduceGrid function.
+    The Solver function will solve a Sudoku Puzzle and find a possible Solution using a Brute-Force (Backtracking) Algorithm.
+
+    To use the Generator more efficiently our GUI we have the Sudoku function.
+    '''
     def __init__(self) -> None:
-        # Initialise empty 9 by 9 grid
+        # Counter variable for reduce Grid
+        self.counter = 1
+
+        # Initialise empty 9x9 grid
         self.grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -14,22 +24,26 @@ class Generator:
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-        # Counter variable for the 
-        self.counter = 1
 
-
-    def checkGrid(self, grid):
-        # This function checks every cell for a value other than zero
+    def checkGrid(self, grid:list) -> bool:
+        '''
+        This function checks every cell for a value other than zero -> validation
+        '''
+        # iter trough grid
         for row in range(0,9):
             for col in range(0,9):
+                # Grid is not solved completely
                 if grid[row][col] == 0:
                     return False
-
-        # The grid is complete 
+        # The grid is complete
         return True
     
     def checkSquare(self, grid:list, row:int, col:int) -> list:
-        #Check in 3x3 Square
+        '''
+        This Function will return a list of the numbers from the 3x3 sqaure in that the cell is located
+        '''
+
+        # Check in 3x3 Square
         c0 = (col//3)*3
         r0 = (row//3)*3
         square = []
@@ -38,11 +52,12 @@ class Generator:
                 square.append(self.grid[r0+r][c0+c]) 
         return square
 
-    def solveGrid(self, grid):
+    def solveGrid(self, grid:list) -> bool:
         # Find next empty cell to solve
         for i in range(0,81):
-            row = i//9 # Needed to get on 9th cell row = 1 and thats the 2nd row of the grid 
-            col = i%9  # No integer divison because columns are not affected by line break 
+            # iter trough grid by integer division for rows and modulo for colums to avoid for-loops and tabs(increases readability)
+            row = i//9
+            col = i%9
             if grid[row][col] == 0:
                 for value in range(1,10):
                     # Check that this number is not alreday used in this row
@@ -61,27 +76,29 @@ class Generator:
                                     break
                                 else:
                                     if self.solveGrid(grid):
-                                        return True 
+                                        return True
+                # all possibilities are done
                 break
+        # Setting back to Zero
         grid[row][col] = 0
 
-    def fillGrid(self):
+    def fillGrid(self) -> bool:
         # Number list needed for the shuffel function
         numberList = [1,2,3,4,5,6,7,8,9]
         # Find next empty cell to solve
         for i in range(0,81):
-            row = i//9 # Needed to get on 9th cell row = 1 and thats the 2nd row of the grid 
-            col = i%9  # No integer divison because columns are not affected by line break 
+            # iter trough grid by integer division for rows and modulo for colums to avoid for-loops and tabs(increases readability)
+            row = i//9
+            col = i%9 
             if self.grid[row][col] == 0:
+                # randomly "sort" 1-9
                 shuffle(numberList)
                 for value in numberList:
                     # Check that this number is not alreday used in this row
                     if value not in self.grid[row]:
                         # Check if this number is not already used in this column
                         if value not in (self.grid[r][col] for r in range(9)):
-
                             # Identify in which of the 9 squares were are working
-                            # Empty square is an a 2D-list 
                             square = self.checkSquare(self.grid, row, col)
                             # Check that this number is not already be used on this 3x3 square
                             if value not in (square):
@@ -91,79 +108,89 @@ class Generator:
                                 else:
                                     if self.fillGrid():
                                         return True 
+                # all possibilities are done
                 break
-        #Setting back to Zero
+        # Setting back to Zero
         self.grid[row][col] = 0
 
-    def print_board(self):
+    def print_board(self) -> None:
+        # Function to print a Game Board not in the list output in the terminal
         for i in range(len(self.grid)):
             if i % 3 == 0 and i != 0:
                 print("- - - - - - - - - - - -  ")
-
             for j in range(len(self.grid[0])):
                 if j % 3 == 0 and j != 0:
                     print(" | ", end="")
-
                 if j == 8:
                     print(self.grid[i][j])
                 else:
                     print(str(self.grid[i][j]) + " ", end="")
 
-    def reduceGrid(self, remaining):
-        # Start by removing numbers from random cells
-        # With increased number of attemps more numbers are getting removed
-        # This increases the difficulty
+    def reduceGrid(self, remaining:int) -> None:
+        '''
+        remaining: int, control difficultness
+
+        This Function will remove numbers from the grid by setting them to 0.
+        The difficult level is controlled by the remaining numbers in the puzzle.
+        17 is the minimum, because it´s the limit for a Puzzle with only one solution, will take a lot of time to generate, Python is not high performing enough.
+        '''
+        # start value of numbers in puzzle, because grid is filled complete
         clues = 81
         while clues >= remaining:
-            #Select a random cell that is not already empty
+            # Select a random cell that is not already empty
             row = randint(0,8)
             col = randint(0,8)
             while self.grid[row][col] == 0:
                 row = randint(0,8)
                 col = randint(0,8)
-            # Remember its cell value in case we need to put it back  
+
+            # Remember its cell value in case we need to put it back, then remove it  
             backup = self.grid[row][col]
             self.grid[row][col] = 0
             
             # Take a full copy of the grid
-            #copyGrid = dc(self.grid)
+            # copyGrid = dc(self.grid)
             copyGrid = []
             for r in range(0,9):
                 copyGrid.append([])
                 for c in range(0,9):
                     copyGrid[r].append(self.grid[r][c])
             
-            # Count the number of solutions that this grid has (using a backtracking approach implemented in the solveGrid() function)
+            # Count the number of solutions that this grid has
             self.counter = 0      
             self.solveGrid(copyGrid)   
             # If the number of solution is different from 1 then we need to cancel the change by putting the value we took away back in the grid
             if self.counter != 1:
                 self.grid[row][col] = backup
-                #We could stop here, but we can also have another attempt with a different cell just to try to remove more numbers
-                #attempts -= 1
+            
+            # one number less in the puzzle
             else:
                 clues -= 1
     
     def sudoku(self, remaining:int)->tuple:
-        #Return Method for Sudoku game, the solved and the game board are returned in a tuple, to do so we need a complete copy
-        #Generatetd Full Board == Solved Board
+        '''
+        remaining: int, control difficultness
+        This Function is a helper so you can create a new Puzzle in different Difficult-Levels by one call.
+        '''
+        # Return Method for Sudoku game, the solved and the game board are returned in a tuple, to do so we need a complete copy
+        # Generatetd Full Board == Solved Board
         self.fillGrid()
         solved_board = []
         for r in range(0,9):
                 solved_board.append([])
                 for c in range(0,9):
                     solved_board[r].append(self.grid[r][c])
-        #Reduce the numbers of the Board, depends on level
+        # Reduce the numbers of the Board, depends on level
         self.reduceGrid(remaining)
         game_board = []
         for r in range(0,9):
                 game_board.append([])
                 for c in range(0,9):
                     game_board[r].append(self.grid[r][c])
-        boards = (solved_board, game_board)
-
-        return(boards)
-
+        
+        return solved_board, game_board
+        # boards = (solved_board, game_board)
+        # return(boards)
 
 def testing():
     # From empty grid to a solvable puzzle
@@ -171,17 +198,23 @@ def testing():
     time_start = time()
     g.fillGrid()
     time_end = time()
-    print(f"Time to fill grid: {time_end - time_start} secs")
+    print(f"Time to generate grid: {time_end - time_start} secs")
     g.print_board()
     print()
     time_start = time()
-    g.reduceGrid()
+    g.reduceGrid(30)
     time_end = time()
     print(f"Time to reduce grid: {time_end - time_start} secs")
     g.print_board()
-    print("Sudoku Grid Ready")
+    count = 0
+    for row in g.grid:
+        for element in row:
+            if element != 0:
+                count += 1
+    
+    print("\nRemaining Numbers:", count)
 
-def boardGenerator(clues):
+def boardGenerator(clues: int) -> tuple:
     g = Generator()
     boards = g.sudoku(clues)
     solved, game = boards
@@ -189,13 +222,4 @@ def boardGenerator(clues):
     return game, solved
 
 if __name__ == "__main__":
-    game, solved = boardGenerator(35)
-    count = 0
-    for i in game:
-        for element in i:
-            if element != 0:
-                count += 1
-    
-    print(count)
-    print('Game: ', game)
-    print('Solved: ', solved)
+    testing()
